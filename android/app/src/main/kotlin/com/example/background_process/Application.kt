f70fd8711e47602d6b901598a64a430754f56b1e
Karthi R <karthir@Karthis-MacBook-Pro.local>
2021-04-13 09:15:23 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/background_process/Application.kt
@@ -0,0 +1,9 @@
+package com.example.background_process
+
+import io.flutter.app.FlutterApplication
+
+class Application : FlutterApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
